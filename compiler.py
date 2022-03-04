51da9e576d3e0c9e44db6ba54582ddf35112839d
Connor Finch <fuxkvalve@email.com>
2022-03-03 18:44:23 -0800

    reorganizing files

---
+++ compiler.py
@@ -0,0 +1,54 @@
+#from qklib.grammar import quack_grammar
+from qklib.wip_grammar import quack_grammar
+import quackgen
+from lark import Lark, Transformer, v_args, Token, Tree
+from os import path
+import argparse
+import json
+from type_checker import TypeChecker
+from astbuilder import ASTBuilder
+from var_checker import VarChecker
+
+def cli():
+    arg_parser = argparse.ArgumentParser(description="Compiles Quack into ASM")
+    arg_parser.add_argument("-f", "--filename", required=True, help="Quack Source File")
+    return arg_parser
+
+def main():
+    # TODO: raw expressions should be immediately popped from the stack as they will never be used
+    args = cli()
+    sourceFilename = vars(args.parse_args())["filename"]
+    if not path.exists(sourceFilename):
+        print("Not a valid file or path to file")
+        args.print_usage()
+        exit()
+
+    quack_parser = Lark(quack_grammar, parser='lalr')
+    sourceFile = open(sourceFilename, "r")
+    text = sourceFile.read()
+    tree = quack_parser.parse(text)
+    #print(tree.pretty("    "))
+    tree = ASTBuilder().transform(tree)
+    #print(tree.pretty("    "))
+    #print(tree.pretty("    "))
+    #print(tree)
+
+    with open("./qklib/builtin_methods.json", "r") as f:
+        types = json.load(f)
+
+    #vc = VarChecker()
+    #vc.visit(tree)
+    VarChecker().visit(tree)
+    #tc = TypeChecker()
+    #tc.visit(tree)
+    TypeChecker().visit(tree)
+
+    g = quackgen.QkGen(types)
+    g.visit(tree)
+    quackgen.build(sourceFilename, g.instructions, g.variables)
+
+    sourceFile.close()
+
+
+if __name__ == '__main__':
+    main()
