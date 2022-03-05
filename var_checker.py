11de04ba3fcf9f498f2d7fc38670f355d6e65a4f
Connor Finch <fuxkvalve@email.com>
2022-03-05 14:11:41 -0800

    attempted to add logic needed to handle var checking inside classes

--- var_checker.py
+++ var_checker.py
@@ -5,10 +5,43 @@ import json
 
 class VarChecker(Visitor_Recursive):
     def __init__(self):
+        # changed to self.variables = {"this"} ?
         self.variables = set()
+        self.constructor = False
+        self.fields = set()
+        self.fields_visited = set()
 
     def visit(self, tree):
-        super().visit(tree)
+        #print(tree.data)
+        if tree.data == "store_field":
+            self.store_field(tree)
+        elif tree.data == "typecase":
+            self.typecase(tree)
+        elif tree.data == "_class":
+            #TODO: reset vars/fields or just fields?
+            """
+            self.variables = set()
+            self.constructor = True  # ?
+            self.class_fields = set()
+            self.class_fields_seen = set()
+            """
+            super().visit(tree)
+        else:
+            super().visit(tree)
+
+    def typecase(self, tree: Tree):
+        pass
+
+    def store_field(self, tree: Tree):
+        if self.constructor:
+            self.fields.add(tree.children[0])
+        self.fields_visited.add(tree.children[0])
+
+    def load_field(self, tree: Tree):
+        if self.constructor:
+            pass
+            #self.class_fields.add()
+        pass
 
     def var(self, tree):
         if tree.children[0] not in self.variables:
@@ -16,15 +49,12 @@ class VarChecker(Visitor_Recursive):
             print(f"NameError: name '{tree.children[0]}' is not defined")
             exit()
 
-    def inf_assignment(self, tree):
-        var_name = tree.children[0]
-        #var_value = tree.children[1]
-        self.variables.add(var_name)
+    def method(self, tree):
+        self.constructor = True if tree.children[0] == "$constructor" else False
+        self.variables = {"this"}
 
     def assignment(self, tree):
         var_name = tree.children[0]
-        #var_type = tree.children[1]
-        #var_value = tree.children[2]
         self.variables.add(var_name)
 
     def if_block(self, tree):
