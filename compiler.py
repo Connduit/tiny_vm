8ef7775ac1513ffcbf63664fe70a28af9c11ce10
Connor Finch <fuxkvalve@email.com>
2022-03-05 14:38:59 -0800

    added visual ast option

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 from qklib.grammar import quack_grammar
 import quackgen
-from lark import Lark, Transformer, v_args, Token, Tree
+from lark import Lark
 from os import path
 import argparse
 import json
@@ -11,14 +11,16 @@ from initializer import ClassInitializer, FieldInitializer
 
 
 def cli():
-    arg_parser = argparse.ArgumentParser(description="Compiles Quack into ASM")
+    arg_parser = argparse.ArgumentParser(description="Compiles Quack into tinyVM")
     arg_parser.add_argument("-f", "--filename", required=True, help="Quack Source File")
+    arg_parser.add_argument("-v", "--visual", action="store_true", help="Print AST to CLI")
     return arg_parser
 
 
 def main():
     args = cli()
     sourceFilename = vars(args.parse_args())["filename"]
+    print_ast = vars(args.parse_args())["visual"]
     if not path.exists(sourceFilename):
         print("Not a valid file or path to file")
         args.print_usage()
@@ -30,7 +32,9 @@ def main():
     tree = quack_parser.parse(text)
     #print(tree.pretty("    "))
     tree = ASTBuilder().transform(tree)
-    #print(tree.pretty("    "))
+
+    if print_ast:
+        print(tree.pretty("    "))
     #print(tree)
 
     with open("./qklib/builtin_methods.json", "r") as f:
